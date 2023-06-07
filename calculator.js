a161e8c12d1a17ac241bb6a98c383f94204b1082
bwill210 <bwill210@fiu.edu>
2023-06-06 18:54:05 -0700

    calculator working with single digit operands only

--- calculator.js
+++ calculator.js
@@ -1,17 +1,19 @@
 function add(a, b) {
-    return console.log(a + b);
+    a = Number(a);
+    b = Number(b);
+    return a + b;
 }
 
 function subtract(a, b) {
-    return console.log(a - b);
+    return a - b;
 }
 
 function multiply(a, b) {
-    return console.log(a * b);
+    return a * b;
 }
 
 function divide(a, b) {
-    return console.log(a / b);
+    return a / b;
 }
 
 function operate(n1, n2, operator) {
@@ -20,7 +22,7 @@ function operate(n1, n2, operator) {
             return add(n1, n2);
         case '-':
             return subtract(n1, n2);
-        case '*':
+        case 'x':
             return multiply(n1, n2);
         case '/':
             return divide(n1, n2);
@@ -29,7 +31,49 @@ function operate(n1, n2, operator) {
     }
 }
 
-let n1 = 4;
-let n2 = 10;
-let operator = '*';
-operate(n1, n2, operator);
+const buttons = document.querySelectorAll("button");
+let displayValue;
+let n1 = undefined;
+let n2 = undefined;
+let currentOperator = undefined;
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.className === "number") {
+            displayValue = button.value;
+            document.querySelector(".display").textContent = displayValue;
+            if (n1 != undefined) {
+                n2 = displayValue;
+            }
+            else {
+                n1 = displayValue;
+            }
+        }
+        else if (button.className === "operand") {
+            if (n2 != undefined) {
+                n1 = operate(n1, n2, currentOperator);
+                n2 = undefined;
+                currentOperator = button.value;
+            }
+            else {
+            currentOperator = button.value;
+            }
+        }
+        else if (button.className === "clear") {
+            displayValue = button.value;
+            document.querySelector(".display").textContent = displayValue;
+            n1 = undefined;
+            n2 = undefined;
+            currentOperator = undefined;
+        }
+        else if (button.className === "equals"){
+            result = operate(n1, n2, currentOperator);
+            console.log(result);
+            document.querySelector(".display").textContent = result;
+            console.log(button.currentOperator);
+            n1 = result;
+            n2 = undefined;
+            currentOperator = undefined;
+        }
+    })
+});
